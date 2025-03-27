f36c59349c874c158ed8b6982108d6bce2a936c5
doxcom <aldoperez1319@gmail.com>
2025-03-26 23:43:29 -0600

    fist changes to project

---
+++ .idea/src/main/java/com/exampleDynamodbaws/DynamoDb_Springboot_crud/DynamoDbSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.exampleDynamodbaws.DynamoDb_Springboot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbSpringbootCrudApplication.class, args);
+	}
+
+}
